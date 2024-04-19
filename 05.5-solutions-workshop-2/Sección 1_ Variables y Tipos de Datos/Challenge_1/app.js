cfdf8cf895a75e81a2b519b7b83e44a4eac2e78a
kevincito1 <67353873+kevincito1@users.noreply.github.com>
2024-04-18 21:14:58 -0500

    fix challenge 1, section 1

--- 05.5-solutions-workshop-2/Sección 1_ Variables y Tipos de Datos/Challenge_1/app.js
+++ 05.5-solutions-workshop-2/Sección 1_ Variables y Tipos de Datos/Challenge_1/app.js
@@ -10,21 +10,24 @@ const entertainmentDaily = 500
 const hotelDaily = 150
 
 let enoughtMoney = (foodDaily + transportDaily + entertainmentDaily + hotelDaily)*time
+console.log(enoughtMoney);
 
 while(budget<= enoughtMoney){
+
     if (budget >= enoughtMoney) {
         alert(`Felicidades Julian no tendras ningún problema con tu viaje, tu presupuesto es de $${budget} y tus gastos estimados seran de $${enoughtMoney}`)
     } else {
         alert(`Julian debes revisar tus actividades, ya que al parecer con tu presupuesto de $${budget} no te alcanzara, ya que tus gastos estimados serian $${enoughtMoney}`)
         time = Number(prompt("Indica la nueva cantidad de días a quedarte"))
         budget = Number(prompt("Indica tu nuevo presupuesto"))
-    }
+        }
 
     enoughtMoney = (foodDaily + transportDaily + entertainmentDaily + hotelDaily)*time
-    alert(`Felicidades Julian no tendras ningún problema con tu viaje, tu presupuesto es de $${budget} y tus gastos estimados seran de $${enoughtMoney}`)
+    if (budget >= enoughtMoney){
+        alert(`Felicidades Julian no tendras ningún problema con tu viaje, tu presupuesto es de $${budget} y tus gastos estimados seran de $${enoughtMoney}`)
+    }
+    console.log(enoughtMoney)
     break;
-
-    
 }
 
 
