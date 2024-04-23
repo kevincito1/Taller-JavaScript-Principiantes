73b654bd82eb92e7cfc3a866e13731740362ff30
kevincito1 <kevin.rodas.cuartas@gmail.com>
2024-04-23 10:25:17 -0500

    Resolve challenge 3, section 2

--- 05.5-solutions-workshop-2/Sección 2_ Truthy y Falsy, Operadores, If-Else/Challenge 3/app.js
+++ 05.5-solutions-workshop-2/Sección 2_ Truthy y Falsy, Operadores, If-Else/Challenge 3/app.js
@@ -1,51 +1,115 @@
-const weather = ["soleado", "lluvia", "nevado"]
-const articlesSunny = [{
+const weather = ["soleado", "lluvia", "nevado"];
+const articlesSunny = [
+  {
     name: "Gafas",
-    weight: 0.2,
-    space: 1
-},
-{
+    weight: 2,
+    space: 2,
+  },
+  {
     name: "Bronceador",
-    weight: 0.3,
-    space: 1 
-},
-{
+    weight: 1,
+    space: 1,
+  },
+  {
     name: "Traje de baño",
-    weight: 0.3,
-    space: 1
-}]
+    weight: 2.5,
+    space: 3,
+  },
+];
 
-const articlesRainy = [{
+const articlesRainy = [
+  {
     name: "Paraguas",
     weight: 2,
-    space: 3
-},
-{
+    space: 3,
+  },
+  {
     name: "Impermeable",
     weight: 2,
-    space: 2
-}]
+    space: 2,
+  },
+];
 
-const articlesSnowy = [{
+const articlesSnowy = [
+  {
     name: "Gorro calefactor",
-    weight: 0.5,
-    space: 1
-},
-{
+    weight: 1,
+    space: 1,
+  },
+  {
     name: "Guantes calefactores",
-    weight: 0.6,
-    space: 1
-},
-{
+    weight: 1.5,
+    space: 1.5,
+  },
+  {
     name: "Chaqueta calefactora",
-    weight: 5,
-    space: 4
+    weight: 3,
+    space: 4,
+  },
+];
 
-}]
+let spaceBagDispo = 6;
+let weightBagDispo = 5;
 
-const spaceBagDispo = 6;
-const weightBagDispo = 10;
+let randomWeather = weather[Math.floor(Math.random() * weather.length)];
 
-articlesSunny.forEach(element => {
-    console.log(element[0])
-});
+console.log(randomWeather);
+
+if (randomWeather === "soleado") {
+  let articlesforSunny = [];
+  for (let j = 0; j <= articlesSunny.length - 1; j++) {
+    articlesforSunny.push(articlesSunny[j].name);
+  }
+    alert("Hola Julian te espera una temporada soleada");
+  while (spaceBagDispo >= 0 && weightBagDispo >=0) {
+    let choice = prompt(`que articulo deseas llevar de los siguientes: 1 ${articlesforSunny[0]} 2 ${articlesforSunny[1]} 3 ${articlesforSunny[2]}`)   
+      if (weightBagDispo >= articlesSunny[choice -1].weight && spaceBagDispo >= articlesSunny[choice -1].space) {
+        weightBagDispo = weightBagDispo - articlesSunny[choice -1].weight;
+        spaceBagDispo = spaceBagDispo - articlesSunny[choice -1].space;
+    }else{
+        alert("No tienes espacio o disponibilidad de peso suficiente para agregar este articulo")
+    }
+    let addArticles = confirm(`Julian aun te queda disponible ${weightBagDispo} de peso y ${spaceBagDispo} de espacio, quieres llevar algo más ?`)
+    if(addArticles !== true ){
+        break;
+    }   
+  }
+}else if(randomWeather === "lluvia"){
+    let articlesforRainy = [];
+    for (let j = 0; j <= articlesRainy.length - 1; j++) {
+      articlesforRainy.push(articlesRainy[j].name);
+    }
+      alert("Hola Julian te espera una temporada de lluvia");
+    while (spaceBagDispo >= 0 && weightBagDispo >=0) {
+      let choice = prompt(`que articulo deseas llevar de los siguientes: 1 ${articlesforRainy[0]} 2 ${articlesforRainy[1]}`)   
+        if (weightBagDispo >= articlesRainy[choice -1].weight && spaceBagDispo >= articlesRainy[choice -1].space) {
+          weightBagDispo = weightBagDispo - articlesRainy[choice -1].weight;
+          spaceBagDispo = spaceBagDispo - articlesRainy[choice -1].space;
+      }else{
+          alert("No tienes espacio o disponibilidad de peso suficiente para agregar este articulo")
+      }
+      let addArticles = confirm(`Julian aun te queda disponible ${weightBagDispo} de peso y ${spaceBagDispo} de espacio, quieres llevar algo más ?`)
+      if(addArticles !== true ){
+          break;
+      }   
+    }
+}else if(randomWeather === "nevado"){
+    let articlesforSnowy = [];
+    for (let j = 0; j <= articlesSnowy.length - 1; j++) {
+      articlesforSnowy.push(articlesSnowy[j].name);
+    }
+      alert("Hola Julian te espera una temporada nevada");
+    while (spaceBagDispo >= 0 && weightBagDispo >=0) {
+      let choice = prompt(`que articulo deseas llevar de los siguientes: 1 ${articlesforSnowy[0]} 2 ${articlesforSnowy[1]} 3 ${articlesforSnowy[2]}`)   
+        if (weightBagDispo >= articlesSnowy[choice -1].weight && spaceBagDispo >= articlesSnowy[choice -1].space) {
+          weightBagDispo = weightBagDispo - articlesSnowy[choice -1].weight;
+          spaceBagDispo = spaceBagDispo - articlesSnowy[choice -1].space;
+      }else{
+          alert("No tienes espacio o disponibilidad de peso suficiente para agregar este articulo")
+      }
+      let addArticles = confirm(`Julian aun te queda disponible ${weightBagDispo} de peso y ${spaceBagDispo} de espacio, quieres llevar algo más ?`)
+      if(addArticles !== true ){
+          break;
+      }   
+}
+}
